014d80e7e66aaec0ee8cf0600eb1e8a9f5ca6131
scarlettc <scarlett.oropeza@gmail.com>
2020-04-01 21:22:07 -0700

    Updates

---
+++ precache-manifest.8086fa34fcaa163179522210251b4c49.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c703432929048e3b2d276d793fd0727a",
+    "url": "/aniversario/index.html"
+  },
+  {
+    "revision": "1142011dfe1f3ef08749",
+    "url": "/aniversario/static/css/main.cb5e8321.chunk.css"
+  },
+  {
+    "revision": "356368876a2e280dd837",
+    "url": "/aniversario/static/js/2.0ae3f4dc.chunk.js"
+  },
+  {
+    "revision": "702e06c21f4db63e7e05a8a0eda9baf1",
+    "url": "/aniversario/static/js/2.0ae3f4dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1142011dfe1f3ef08749",
+    "url": "/aniversario/static/js/main.c08e7015.chunk.js"
+  },
+  {
+    "revision": "474387c6f3a284a4c73b",
+    "url": "/aniversario/static/js/runtime-main.e77b7bbc.js"
+  },
+  {
+    "revision": "43071194d1d32ffc2197050f2ee0d697",
+    "url": "/aniversario/static/media/img-big.43071194.jpg"
+  },
+  {
+    "revision": "1853c0fbf9eec1d9e545af7789b3f42e",
+    "url": "/aniversario/static/media/img-cumple.1853c0fb.jpg"
+  },
+  {
+    "revision": "17a9ddd9964533e14d05650aca4d4b26",
+    "url": "/aniversario/static/media/img-love.17a9ddd9.jpg"
+  }
+]);
